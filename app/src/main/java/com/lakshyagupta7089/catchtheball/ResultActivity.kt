455932d12e65f7a3f94fc19b2ae4b12ba512545d
Lakshya Coder <lakshyagupta7089@gmail.com>
2021-11-25 18:57:51 +0530

    first commit

--- app/src/main/java/com/lakshyagupta7089/catchtheball/ResultActivity.kt
+++ app/src/main/java/com/lakshyagupta7089/catchtheball/ResultActivity.kt
@@ -2,10 +2,12 @@ package com.lakshyagupta7089.catchtheball
 
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lakshyagupta7089.catchtheball.databinding.ActivityResultBinding
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
@@ -36,10 +38,28 @@ class ResultActivity : AppCompatActivity() {
             editor.putInt("HIGH_SCORE", score)
             editor.apply()
 
-            binding.highScoreLabel.text = getString(R.string.result_high_score, score)
+            binding.highScoreLabel.text = getString(R.string.new_result_high_score, score)
+
+            // Show confetti
+            binding.viewKonfetti.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square, Shape.Circle)
+                .addSizes(Size(12))
+                .setPosition(-50f, binding.viewKonfetti.width + 50f, -50f, -50f)
+                .streamFor(300, 5000L)
         } else {
             binding.highScoreLabel.text = getString(R.string.result_high_score, highScore)
         }
+
+//        CommonConfetti.rainingConfetti(binding.root, intArrayOf(
+//            Color.YELLOW,
+//            Color.GREEN,
+//            Color.MAGENTA,
+//        )).oneShot()
 
         // Setting up onClickListener on tryAgainButton
         binding.tryAgainButton.setOnClickListener { tryAgain() }
